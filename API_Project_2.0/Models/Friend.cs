40a8d23da588108fbd6748edab173060402dcad0
Jonathan Gilmer <jonathan@jonathangilmer.com>
2022-06-01 15:42:10 -0400

    initial commit of API Project 2.0

---
+++ API_Project_2.0/Models/Friend.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace API_Project_2._0.Models
+{
+    public class Friend
+    {
+
+        // Friend Default Constructor
+        public Friend()
+        {
+        }
+
+        // Friend Paramaterized Constructor
+        public Friend(int id, string firstName, string lastName, string location, DateTime dateOfHire)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.location = location;
+            this.dateOfHire = dateOfHire;
+        }
+
+        // Friend Properties
+        private int id { get; set; }
+        private string firstName { get; set; }
+        private string lastName { get; set; }
+        private string location { get; set; }
+        private DateTime dateOfHire { get; set; }
+    }
+}
